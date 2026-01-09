c29737b3f1838cd70d4b63cfa7863ff2ec2a920c
blickdai3690 <blickdai3690@gmail.com>
2026-01-09 15:06:41 +0100

    Modify conditions treasure island

--- day_03/treasure_island.py
+++ day_03/treasure_island.py
@@ -20,16 +20,10 @@ print("Your mission is to find the treasure")
 print("You're at a cross road. Where do you want to go?")
 choice1 = input("    Type \"left\" or \"right\"\n")
 
-if choice1 == "right":
-    print("Fall into a hole.")
-    print("Game Over.")
-elif choice1 == "left":
+if choice1 == "left":
     print("You've come to a lake. There is an island in the middle of the lake")
     choice2 = input("    Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n")
-    if choice2 == "swim":
-        print("Attacked by trout.")
-        print("Game Over.")
-    elif choice2 == "wait":
+    if choice2 == "wait":
         print("You've come to a castle. There are three doors in the castle")
         choice3 = input("    Type \"red\" to open the red door, \"yellow\" for the yellow one, \"blue\" for the blue one .\n")
         if choice3 == "red":
@@ -39,4 +33,12 @@ elif choice1 == "left":
             print("You Win!")
         elif choice3 == "blue":
             print("Eaten by beasts.")
-            print("Game Over.")   
+            print("Game Over.")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by trout.")
+        print("Game Over.")
+else:
+    print("Fall into a hole.")
+    print("Game Over.")
